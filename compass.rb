40b282aabbfe05e47355582deeaced50e4a21452
Nathan Brettell <nathanbrettell@gmail.com>
2014-11-02 15:26:23 +0000

    Base of portfolio

---
+++ compass.rb
@@ -0,0 +1,19 @@
+# config.rb
+
+# -----------------------------------------------
+# Paths
+# -----------------------------------------------
+
+http_path    = "/"
+project_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+css_dir      = "output/assets/styles"
+sass_dir     = "content/assets/styles"
+images_dir   = "output/assets/images"
+
+# -----------------------------------------------
+# Output
+# -----------------------------------------------
+
+output_style = :compressed
+preferred_syntax = :scss
+relative_assets = true
