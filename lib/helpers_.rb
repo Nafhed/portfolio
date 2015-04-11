f6e8618906da3466bece4fd860a4c93f5ce9aec7
Nathan Brettell <nathanbrettell@gmail.com>
2015-04-11 13:17:34 +0100

    Nanoc Toolbox

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -4,8 +4,13 @@
 include LipsumPlaceHolder
 # include DateTimeHandler
 
+require "nanoc/toolbox"
+
 # include Nanoc::Filters::Mustache
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+# Helpers of the nanoc-toolbox
+include Nanoc::Toolbox::Helpers::GoogleAnalytics
